0e4b22be8a93c17ff5acb23dbe222d1fe6d4831a
root <nikoe14@gmail.com>
2016-07-15 17:39:02 +0000

    exit 0

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 cd /var/lib/jenkins/workspace/test/my-app/target;
 if [ -f my-app-1.0-SNAPSHOT.war ]; then
   scp my-app-1.0-SNAPSHOT.war root@172.17.0.4:/usr/local/tomcat/webapps/myapp.war
+  exit 0
 fi
 cd -
